d18353c310e92d444bdf1313183ea24f19e9353c
VolodymyrLys <38280081+VolodymyrLys@users.noreply.github.com>
2018-04-25 13:25:39 +0300

    Add files via upload

---
+++ counter a_b.py
@@ -0,0 +1,15 @@
+def counter(a, b):
+    a_str = list(str(a))
+    b_str = list(str(b))
+    count = 0
+    for i in b_str:
+        if i in a_str:
+            count +=1
+
+    return(count)
+
+
+print (counter(12345, 567))
+print (counter(1233211, 12128))
+print (counter(123, 45))
+
